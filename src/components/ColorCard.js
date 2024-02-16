ec8094facf9fa05edb71e666529a29d6b5300e65
Ebenezer Arthur <arthurebenezer@aol.com>
2024-02-16 14:21:13 +0000

    fixed black instance

--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const ColorCard = ({ color }) => {
+  const isBlack = color && color.toLowerCase() === "black";
+  const textColor = isBlack ? "white" : "black";
   const displayColor = color ? color : "Empty Value";
+
   return (
-    <div className="colorCard" style={{ backgroundColor: color }}>
+    <div
+      className="colorCard"
+      style={{ backgroundColor: color, color: textColor }}
+    >
       {displayColor}
     </div>
   );
